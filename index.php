2ddd8c8e2be67d487a485860ac3df29f4d1e97ef
jlsodai <devlessco@gmail.com>
2018-01-02 10:38:18 +0000

    change to php... extract files

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Kuza App</title>
+
+        <link href="assets/plugins/switchery/switchery.min.css" rel="stylesheet" />
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]--><!-- Modernizr js -->
+        <script src="assets/js/modernizr.min.js"></script>
+
+    </head>
+
+
+    <body class="fixed-left">
+        <div id="wrapper"> <!-- Begin page -->
+            <?php include('top-bar.php'); ?>
+            <?php include('left-sidebar.php'); ?>
+
+            <div class="content-page">
+                <div class="content"><!-- Start content -->
+                    <div class="container">
+                        <div class="row">
+							<div class="col-xs-12">
+								<div class="page-title-box">
+                                    <h4 class="page-title">Page Title</h4>
+                                    <ol class="breadcrumb p-0">
+                                        <li><a href="#">Kuza App</a></li>
+                                        <li><a href="#">Pages</a></li>
+                                        <li class="active">Starter Page</li>
+                                    </ol>
+                                    <div class="clearfix"></div>
+                                </div>
+							</div>
+						</div><!-- end row -->
+                    </div> <!-- container -->
+                </div> <!-- content -->
+            </div><!-- End content-page -->
+
+            <footer class="footer text-right">
+                2018 © Kuza App.
+            </footer>
+        </div><!-- END wrapper -->
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/tether.min.js"></script><!-- Tether for Bootstrap -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/plugins/switchery/switchery.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+        
+        <script src="assets/js/custom.js"></script>
+
+    </body>
+</html>
---
+++ left-sidebar.php
@@ -0,0 +1,33 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left side-menu">
+    <div class="sidebar-inner slimscrollleft">
+        <div id="sidebar-menu"><!--- Sidemenu -->
+            <ul>
+                <li class="text-muted menu-title">Navigation</li>
+                <li class="has_sub">
+                    <a href="index.html" class="waves-effect"><span class="label label-pill label-primary pull-xs-right">1</span><i class="zmdi zmdi-view-dashboard"></i><span> Dashboard </span> </a>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-format-underlined"></i> <span> Products </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="javascript:void(0);">List Products</a></li>
+                        <li><a href="javascript:void(0);">Add Product</a></li>
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-album"></i> <span> Orders </span> <span class="menu-arrow"></span></a>
+                    <ul class="list-unstyled">
+                        <li><a href="javascript:void(0);">All Orders</a></li>
+                        <li><a href="javascript:void(0);">Processed</a></li>
+                        <li><a href="javascript:void(0);">Non Processed</a></li>
+                    </ul>
+                </li>
+                <li class="has_sub">
+                    <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-settings"></i><span> Settings </span> </a>
+                </li>
+            </ul>
+            <div class="clearfix"></div>
+        </div><!-- Sidebar -->
+        <div class="clearfix"></div>
+    </div>
+</div><!-- Left Sidebar End -->
