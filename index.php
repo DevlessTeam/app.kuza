5311e1839603ad3e5229f762156c91d25acf4c1a
jlsodai <devlessco@gmail.com>
2018-01-09 13:46:39 +0000

    dashboard -> orders

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Kuza App</title>
 
+        <link href="assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        
+        <link href="assets/plugins/custombox/css/custombox.min.css" rel="stylesheet">
         <link href="assets/plugins/switchery/switchery.min.css" rel="stylesheet" />
         <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
         <link href="assets/css/custom.css" rel="stylesheet" type="text/css" />
@@ -21,29 +26,176 @@
             <?php include('left-sidebar.php'); ?>
 
             <div class="content-page">
-                <div class="content"><!-- Start content -->
+                <!-- Start content -->
+                <div class="content">
                     <div class="container">
+
                         <div class="row">
-							<div class="col-xs-12">
-								<div class="page-title-box">
-                                    <h4 class="page-title">Dashboard</h4>
+                            <div class="col-xs-12">
+                                <div class="page-title-box">
+                                    <h4 class="page-title">Orders</h4>
                                     <ol class="breadcrumb p-0">
-                                        <li><a href="#">Kuza App</a></li>
-                                        <li><a href="#">Pages</a></li>
-                                        <li class="active">Starter Page</li>
+                                        <li>
+                                            <a href="#">Uplon</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Tables</a>
+                                        </li>
+                                        <li class="active">
+                                            Orders
+                                        </li>
                                     </ol>
                                     <div class="clearfix"></div>
                                 </div>
-							</div>
-						</div><!-- end row -->
+                            </div>
+                        </div>
+                        <!-- end row -->
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card-box table-responsive">
+                                    <?php include('orders-table.php'); ?>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end row -->
+
                     </div> <!-- container -->
+
                 </div> <!-- content -->
-            </div><!-- End content-page -->
+
+            </div>
+            <!-- End content-page -->
+
+
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
+
+
+            <!-- Right Sidebar -->
+            <div class="side-bar right-bar">
+                <div class="nicescroll">
+                    <ul class="nav nav-tabs text-xs-center">
+                        <li class="nav-item">
+                            <a href="#home-2"  class="nav-link active" data-toggle="tab" aria-expanded="false">
+                                Activity
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#messages-2" class="nav-link" data-toggle="tab" aria-expanded="true">
+                                Settings
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="home-2">
+                            <div class="timeline-2">
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">5 minutes ago</small>
+                                        <p><strong><a href="#" class="text-info">John Doe</a></strong> Uploaded a photo <strong>"DSC000586.jpg"</strong></p>
+                                    </div>
+                                </div>
+
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">30 minutes ago</small>
+                                        <p><a href="" class="text-info">Lorem</a> commented your post.</p>
+                                        <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
+                                    </div>
+                                </div>
+
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">59 minutes ago</small>
+                                        <p><a href="" class="text-info">Jessi</a> attended a meeting with<a href="#" class="text-success">John Doe</a>.</p>
+                                        <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
+                                    </div>
+                                </div>
+
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">1 hour ago</small>
+                                        <p><strong><a href="#" class="text-info">John Doe</a></strong>Uploaded 2 new photos</p>
+                                    </div>
+                                </div>
+
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">3 hours ago</small>
+                                        <p><a href="" class="text-info">Lorem</a> commented your post.</p>
+                                        <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
+                                    </div>
+                                </div>
+
+                                <div class="time-item">
+                                    <div class="item-info">
+                                        <small class="text-muted">5 hours ago</small>
+                                        <p><a href="" class="text-info">Jessi</a> attended a meeting with<a href="#" class="text-success">John Doe</a>.</p>
+                                        <p><em>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam laoreet tellus ut tincidunt euismod. "</em></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="messages-2">
+
+                            <div class="row m-t-20">
+                                <div class="col-xs-8">
+                                    <h5 class="m-0">Notifications</h5>
+                                    <p class="text-muted m-b-0"><small>Do you need them?</small></p>
+                                </div>
+                                <div class="col-xs-4 text-right">
+                                    <input type="checkbox" checked data-plugin="switchery" data-color="#64b0f2" data-size="small"/>
+                                </div>
+                            </div>
+
+                            <div class="row m-t-20">
+                                <div class="col-xs-8">
+                                    <h5 class="m-0">API Access</h5>
+                                    <p class="m-b-0 text-muted"><small>Enable/Disable access</small></p>
+                                </div>
+                                <div class="col-xs-4 text-right">
+                                    <input type="checkbox" checked data-plugin="switchery" data-color="#64b0f2" data-size="small"/>
+                                </div>
+                            </div>
+
+                            <div class="row m-t-20">
+                                <div class="col-xs-8">
+                                    <h5 class="m-0">Auto Updates</h5>
+                                    <p class="m-b-0 text-muted"><small>Keep up to date</small></p>
+                                </div>
+                                <div class="col-xs-4 text-right">
+                                    <input type="checkbox" checked data-plugin="switchery" data-color="#64b0f2" data-size="small"/>
+                                </div>
+                            </div>
+
+                            <div class="row m-t-20">
+                                <div class="col-xs-8">
+                                    <h5 class="m-0">Online Status</h5>
+                                    <p class="m-b-0 text-muted"><small>Show your status to all</small></p>
+                                </div>
+                                <div class="col-xs-4 text-right">
+                                    <input type="checkbox" checked data-plugin="switchery" data-color="#64b0f2" data-size="small"/>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div> <!-- end nicescroll -->
+            </div>
+            <!-- /Right-bar -->
 
             <footer class="footer text-right">
-                2018 © Kuza App.
+                2016 © Uplon.
             </footer>
-        </div><!-- END wrapper -->
+
+
+        </div>
+        <!-- END wrapper -->
+
 
         <script>
             var resizefunc = [];
@@ -62,11 +214,44 @@
         <script src="assets/js/jquery.slimscroll.js"></script>
         <script src="assets/plugins/switchery/switchery.min.js"></script>
 
-        <!-- App js -->
+        <!-- Required datatable js -->
+        <script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+        <script src="assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+        <!-- Buttons examples -->
+        <script src="assets/plugins/datatables/dataTables.buttons.min.js"></script>
+        <script src="assets/plugins/datatables/buttons.bootstrap4.min.js"></script>
+        <script src="assets/plugins/datatables/jszip.min.js"></script>
+        <script src="assets/plugins/datatables/pdfmake.min.js"></script>
+        <script src="assets/plugins/datatables/vfs_fonts.js"></script>
+        <script src="assets/plugins/datatables/buttons.html5.min.js"></script>
+        <script src="assets/plugins/datatables/buttons.print.min.js"></script>
+        <script src="assets/plugins/datatables/buttons.colVis.min.js"></script>
+        <!-- Responsive examples -->
+        <script src="assets/plugins/datatables/dataTables.responsive.min.js"></script>
+        <script src="assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
+
+        <!-- Modal-Effect -->
+        <script src="assets/plugins/custombox/js/custombox.min.js"></script>
+        <script src="assets/plugins/custombox/js/legacy.min.js"></script>
+
         <script src="assets/js/jquery.core.js"></script>
         <script src="assets/js/jquery.app.js"></script>
-        
-        <script src="assets/js/custom.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                $('#datatable').DataTable();
+
+                //Buttons examples
+                var table = $('#datatable-buttons').DataTable({
+                    lengthChange: false,
+                    buttons: ['copy', 'excel', 'pdf', 'colvis']
+                });
+
+                table.buttons().container()
+                        .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+            } );
+
+        </script>
 
     </body>
 </html>
