c08997cf8a9587ea906e0aab7819e46e50bee2f1
jlsodai <devlessco@gmail.com>
2018-01-23 06:40:41 +0000

    modify products links to form links

--- left-sidebar.php
+++ left-sidebar.php
@@ -8,10 +8,10 @@
                     <a href="index.php" class="waves-effect"><span class="label label-pill label-primary pull-xs-right">1</span><i class="zmdi zmdi-view-dashboard"></i><span> Dashboard </span> </a>
                 </li>
                 <li class="has_sub">
-                    <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-format-underlined"></i> <span> Products </span> <span class="menu-arrow"></span></a>
+                    <a href="javascript:void(0);" class="waves-effect"><i class="zmdi zmdi-format-underlined"></i> <span> KuzaForms </span> <span class="menu-arrow"></span></a>
                     <ul class="list-unstyled">
-                        <li><a href="javascript:void(0);">List Products</a></li>
-                        <li><a href="javascript:void(0);">Add Product</a></li>
+                        <li><a href="forms.php">All Forms</a></li>
+                        <li><a href="single-form.php">Add Form</a></li>
                     </ul>
                 </li>
                 <li class="has_sub">
